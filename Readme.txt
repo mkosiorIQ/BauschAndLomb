1dd2006b7dc2aa8904a8eaf7f76a29019223c8bf
Mike-Kosior <mkosior@iq-inc.com>
2026-01-15 15:21:13 -0500

    Added missing updates.

--- Readme.txt
+++ Readme.txt
@@ -15,3 +15,48 @@ Git commands in your terminal, replacing the URL with your repository's actual U
     git remote add origin https://github.com/USERNAME/REPOSITORY_NAME.git
     git push origin main
     git pull origin main
+Step 3: In repro create a folder ./githib/workflows/
+Step 4: In the folder create a YAML file with .yml extension
+
+
+-----------------------------------------------------------------------------
+Test on a "committed" file
+gh workflow run "Deploy Terraform" --ref main
+    the file with 'name: Deploy Terraform' must be committed
+gh run list --workflow="deploy-terraform.yml"
+gh run view 20928730072 --log
+-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------
+Test on a github workflow locally on windows before a commit
+Note act uses Docker
+act -P windows-latest=-self-hosted -j terraform-plan
+
+Remove locked directories under act
+C:\Users\mkosior\.cache\act
+
+export AZURE_SP_CREDENTIALS='{ "clientId": "...", "clientSecret": "...", "subscriptionId": "...", "tenantId": "..." }'
+export AZURE_SP_CREDENTIALS='{ "subscriptionId": "046696af-1d89-4ff1-9ab1-411f666c1c06", "tenantId": "38e87a3b-2695-4888-9f38-f0feeed23c9f", "clientId": "3fc42ced-ec65-49ff-a5fa-548b14649804", "clientSecret": "je18Q~VJ2kGR6tBmySfHZwrojD6DOPoYOsDgrdwc" }'
+act -s AZURE_SP_CREDENTIALS
+Directly in command
+act -s AZURE_SP_CREDENTIALS='{ "subscriptionId": "046696af-1d89-4ff1-9ab1-411f666c1c06", "tenantId": "38e87a3b-2695-4888-9f38-f0feeed23c9f", "clientId": "3fc42ced-ec65-49ff-a5fa-548b14649804", "clientSecret": "je18Q~VJ2kGR6tBmySfHZwrojD6DOPoYOsDgrdwc" }'
+or .secrets in your repository root or a secure location
+
+act -P windows-latest=-self-hosted --secret-file .secrets -j terraform-plan
+-----------------------------------------------------------------------------
+
+
+-----------------------------------------------------------------------------
+To run Terraform in the terraform directory
+$env:ARM_SUBSCRIPTION_ID = "046696af-1d89-4ff1-9ab1-411f666c1c06"
+$env:ARM_TENANT_ID = "38e87a3b-2695-4888-9f38-f0feeed23c9f"
+$env:ARM_CLIENT_ID = "3fc42ced-ec65-49ff-a5fa-548b14649804"
+$env:ARM_CLIENT_SECRET = "je18Q~VJ2kGR6tBmySfHZwrojD6DOPoYOsDgrdwc"
+
+$env:TF_VAR_SUBSCRIPTION_ID = "046696af-1d89-4ff1-9ab1-411f666c1c06"
+-----------------------------------------------------------------------------
+
+
+-----------------------------------------------------------------------------
+May need az login to be able to pass two stage authentication.
+-----------------------------------------------------------------------------
--- terraform/config.tf
+++ terraform/config.tf
@@ -17,12 +17,64 @@ resource "random_integer" "suffix" {
   max = 99999
 }
 
+# Create Azure Resource Group for Monitoring Resources
 resource "azurerm_resource_group" "rg" {
   name     = "bl-monitoring-rg1"
   location = "East US"
 }
 
-# IoT Hub
+# Create Azure Storage Account (ADLS Gen2) for Telemetry Data
+resource "azurerm_storage_account" "st" {
+  name                     = "blmonitoring${random_integer.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+  min_tls_version          = "TLS1_2"
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Create Azure Storage Container for Telemetry Data
+resource "azurerm_storage_container" "telemetry" {
+  name                  = "telemetry"
+  storage_account_id    = azurerm_storage_account.st.id # Reference the ID
+  container_access_type = "private"
+}
+
+# Create Azure Evvent Hub Namespace for Telemetry Ingestion
+resource "azurerm_eventhub_namespace" "namespace" {
+  name                = "bl-eh-namespace-${random_integer.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Basic"
+  capacity            = 1
+}
+
+# Create Azure Event Hub for Telemetry Ingestion
+resource "azurerm_eventhub" "eventhub" {    
+  name                = "bl-telemetry-eh-${random_integer.suffix.result}"
+  namespace_name      = azurerm_eventhub_namespace.namespace.name
+  resource_group_name = azurerm_resource_group.rg.name
+  partition_count     = 2
+  message_retention   = 1
+}
+
+# Create Azure Event Hub Authorization Rule for Telemetry Ingestion
+resource "azurerm_eventhub_authorization_rule" "auth_rule" {
+  name                = "bl-eh-auth-rule-${random_integer.suffix.result}"
+  namespace_name      = azurerm_eventhub_namespace.namespace.name
+  eventhub_name       = azurerm_eventhub.eventhub.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  listen = true
+  send   = true
+  manage = false
+}
+
+# Create Azure IoT Hub for Device Communication
 resource "azurerm_iothub" "iothub" {
   name                = "bl-iothub-${random_integer.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -34,6 +86,38 @@ resource "azurerm_iothub" "iothub" {
   }
 
   tags = {
+    purpose = "testing-azure-iothub"
     environment = "dev"
+  }
+
+  endpoint {  
+    name                       = "export-storage-endpoint"
+    type                       = "AzureIotHub.StorageContainer"
+    connection_string          = azurerm_storage_account.st.primary_connection_string
+    container_name             = azurerm_storage_container.telemetry.name
+    batch_frequency_in_seconds = 60
+  } 
+
+  endpoint {
+    name                       = "export-eventhub-endpoint"
+    type                       = "AzureIotHub.EventHub"
+    connection_string          = azurerm_eventhub_authorization_rule.auth_rule.primary_connection_string
+    batch_frequency_in_seconds = 60
+  }
+
+  route {
+    name                   = "export-telemetry-route-storage"
+    source                 = "DeviceMessages"
+    endpoint_names         = ["export-storage-endpoint"]
+    enabled                = true
+    condition              = "true"
+  }
+
+  route {
+    name                   = "export-telemetry-route-eventhub"
+    source                 = "DeviceMessages"
+    endpoint_names         = ["export-eventhub-endpoint"]
+    enabled                = true
+    condition              = "true"
   }
 }
